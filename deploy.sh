4f9c170919f1c6c5e2ac87e9a431c3e3dc33eee5
King <yongchao.wang@ingka.com>
2022-02-16 22:43:27 +0800

    build

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,13 @@ yarn build
 git add .
 git commit -m 'build'
 git push
+
+sudo cp -R  "/Users/king/Documents/GitHub/arDemo/dist" "/Users/king/Desktop/dist"
+
+git checkout "gh-pages"
+
+sudo cp -R  "/Users/king/Desktop/dist" "/Users/king/Documents/GitHub/arDemo" 
+
+git add .
+git commit -m 'release'
+git push
