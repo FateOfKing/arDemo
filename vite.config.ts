375e97cdda6a0d8dce34d4f913f9065dee38a9dd
King <yongchao.wang@ingka.com>
2022-02-17 09:53:48 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode })=>{
+  console.log('command',command);
+  
  return{ 
    plugins: [vue()],
-   base: command === 'serve'? './' :'/arDemo/'
+   base: command === 'serve'? '/' :'/arDemo/'
 }
 })
